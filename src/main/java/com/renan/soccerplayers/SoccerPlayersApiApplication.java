d30d8c99e178a017e12881587f828b84d76dd15c
Renan Cadaval <renancadaval@gmail.com>
2022-05-03 21:11:08 -0300

    Initial commit

---
+++ src/main/java/com/renan/soccerplayers/SoccerPlayersApiApplication.java
@@ -0,0 +1,13 @@
+package com.renan.soccerplayers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerPlayersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerPlayersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renan/soccerplayers/SoccerPlayersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renan.soccerplayers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerPlayersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
